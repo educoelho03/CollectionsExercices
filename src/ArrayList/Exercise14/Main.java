7f110f4a6c4dbd9dd45d473ee8900a9c1b347a7a
ecoelho03 <ecoelho@contanciainvest.com.br>
2023-06-30 10:53:32 -0300

    Exercicio resolvido

---
+++ src/ArrayList/Exercise14/Main.java
@@ -0,0 +1,22 @@
+package ArrayList.Exercise14;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> c1= new ArrayList<>();
+        c1.add("Red");
+        c1.add("Green");
+        c1.add("Black");
+        c1.add("White");
+        c1.add("Pink");
+
+
+        System.out.println("Lista antes da troca: " + c1);
+
+        Collections.swap(c1, 1, 3);
+
+        System.out.println("Lista depois da troca: " + c1);
+    }
+}
---
+++ src/ArrayList/Exercise15/Main.java
@@ -0,0 +1,30 @@
+package ArrayList.Exercise15;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> c1= new ArrayList<>();
+        c1.add("Red");
+        c1.add("Green");
+        c1.add("Black");
+        c1.add("White");
+        c1.add("Pink");
+
+        ArrayList<String> c2= new ArrayList<>();
+        c2.add("Blue");
+        c2.add("Grey");
+        c2.add("Yellow");
+        c2.add("Purple");
+
+
+        ArrayList<String> a = new ArrayList<>();
+
+        a.addAll(c1);
+        a.addAll(c2);
+
+        System.out.println("Lista unida: " + a);
+
+    }
+}
